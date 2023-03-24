960f18b1586d1f0d4e852277d6dc8d7395c1b7f1
Emil Karimov <emvakar@gmail.com>
2023-03-24 15:07:12 +0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# FJWTMiddleware
+
+A description of this package.
---
+++ Sources/FJWTMiddleware/FJWTMiddleware.swift
@@ -0,0 +1,6 @@
+public struct FJWTMiddleware {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
